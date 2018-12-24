7232c218db9a3d7fd42256856fab4872f2219458
Michael Tarleton <michael.tarleton@me.com>
2018-12-24 14:30:12 -0600

    master: Added random names and purchase items

--- TripCalculatorService/Entities/Friend.cs
+++ TripCalculatorService/Entities/Friend.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace TripCalculatorService.Entities
@@ -6,6 +7,32 @@ namespace TripCalculatorService.Entities
     {
         public string Name { get; set; }
 
-        public List <PurchaseItem> AmountsPaid { get; set; }
+        public List <PurchaseItem> PurchasedItems { get; set; }
+
+        public static Friend RandomFriend()
+        {
+            Random random = new Random();
+
+            string[]       names = { "Sam", "Fred", "James", "John", "Sarah", "Jill" };
+            PurchaseItem[] items =
+            {
+                new PurchaseItem { Name = "Chips",     Price = 8.00M },
+                new PurchaseItem { Name = "Cheese",    Price = 8.00M },
+                new PurchaseItem { Name = "Soda",      Price = 8.00M },
+                new PurchaseItem { Name = "Chocolate", Price = 8.00M }
+            };
+
+            string       randomName     = names[random.Next(0, names.Length - 1)];
+            PurchaseItem randomPurchase = items[random.Next(0, items.Length - 1)];
+
+            Friend friend = new Friend {
+                Name           = randomName,
+                PurchasedItems = new List <PurchaseItem> {
+                    randomPurchase
+                }
+            };
+
+            return(friend);
+        }
     }
 }
