f798935994b093d752c61eddcd2cc431ac9d312b
Michael Tarleton <michael.tarleton@me.com>
2018-12-24 14:30:41 -0600

    master: Minor name change

--- TripCalculatorService/Configuration/ConfigurationExtensions.cs
+++ TripCalculatorService/Configuration/ConfigurationExtensions.cs
@@ -69,6 +69,8 @@ namespace TripCalculatorService.Configuration
                         )
                     )
                 ));
+
+            client.CreateDocument(Friend.BuildRandomFriend());
         }
     }
 }
