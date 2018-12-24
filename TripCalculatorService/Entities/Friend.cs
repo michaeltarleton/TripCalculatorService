91a306ae56679310b53e1a9823129a51c8b5118d
Michael Tarleton <michael.tarleton@me.com>
2018-12-24 15:11:29 -0600

    master: No duplicate random people

--- TripCalculatorService/Entities/Friend.cs
+++ TripCalculatorService/Entities/Friend.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TripCalculatorService.Entities
 {
-    public class Friend : BaseEntity
+    public class Friend : BaseEntity, IEqualityComparer <Friend>
     {
         public string Name { get; set; }
 
@@ -34,5 +36,21 @@ namespace TripCalculatorService.Entities
 
             return(friend);
         }
+
+        public static IEnumerable <Friend> BuildRandomFriends(int maxFrineds = 10)
+        {
+            List <Friend> friends = new List <Friend>();
+
+            for (int i = 0; i < maxFrineds; i++)
+            {
+                friends[i] = Friend.BuildRandomFriend(i);
+            }
+
+            return(friends.Distinct());
+        }
+
+        public bool Equals(Friend x, Friend y) { return(x.GetHashCode() == y.GetHashCode()); }
+
+        public int GetHashCode(Friend obj) { return(HashCode.Combine(obj.Id, obj.Name)); }
     }
 }
