9638a38b25754af9f33fddc3fc03d9722f81df58
Michael Tarleton <michael.tarleton@me.com>
2018-12-25 20:34:51 -0600

    master: Moved purchased item mapper into its own file

--- TripCalculatorService/Mappers/FriendMappers.cs
+++ TripCalculatorService/Mappers/FriendMappers.cs
@@ -33,35 +33,4 @@ namespace TripCalculatorService.Mappers
             return models == null ? null : models.Select(m => m.ToEntity());
         }
     }
-
-    public static class PurchasedItemMappers
-    {
-        public static Models.PurchasedItem ToModel(this Entities.PurchasedItem purchasedItem)
-        {
-            return purchasedItem == null ? null : new Models.PurchasedItem {
-                       Id    = purchasedItem.Id,
-                       Name  = purchasedItem.Name,
-                       Price = purchasedItem.Price
-            };
-        }
-
-        public static IEnumerable<Models.PurchasedItem> ToModel(this IEnumerable<Entities.PurchasedItem> purchaseItem)
-        {
-            return purchaseItem == null ? null : purchaseItem.Select(i => i.ToModel());
-        }
-
-        public static Entities.PurchasedItem ToEntity(this Models.PurchasedItem purchasedItem)
-        {
-            return purchasedItem == null ? null : new Entities.PurchasedItem {
-                       Id    = purchasedItem.Id,
-                       Name  = purchasedItem.Name,
-                       Price = purchasedItem.Price
-            };
-        }
-
-        public static IEnumerable<Entities.PurchasedItem> ToEntity(this IEnumerable<Models.PurchasedItem> purchasedItems)
-        {
-            return purchasedItems == null ? null : purchasedItems.Select(i => i.ToEntity());
-        }
-    }
 }
