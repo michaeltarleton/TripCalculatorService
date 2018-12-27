f450e2e7c4c89e513c839db644e0ff623579b9da
Michael Tarleton <michael.tarleton@me.com>
2018-12-26 20:04:07 -0600

    master: Added null reference check on data access response models

--- TripCalculatorService/Mappers/DataAccessResponseMappers.cs
+++ TripCalculatorService/Mappers/DataAccessResponseMappers.cs
@@ -7,26 +7,31 @@ namespace TripCalculatorService.Mappers
     {
         public static DataAccessResponse<Models.Friend> ToModel(this DataAccessResponse<Entities.Friend> response)
         {
+            if (response == null || response.Payload == null) return null;
             return new DataAccessResponse<Models.Friend>(response.Payload.ToModel(), response.Status);
         }
 
         public static DataAccessResponse<IEnumerable<Models.Friend>> ToModel(this DataAccessResponse<IEnumerable<Entities.Friend>> response)
         {
+            if (response == null || response.Payload == null) return null;
             return new DataAccessResponse<IEnumerable<Models.Friend>>(response.Payload.ToModel(), response.Status);
         }
 
         public static DataAccessResponse<Models.PurchasedItem> ToModel(this DataAccessResponse<Entities.PurchasedItem> response)
         {
+            if (response == null || response.Payload == null) return null;
             return new DataAccessResponse<Models.PurchasedItem>(response.Payload.ToModel(), response.Status);
         }
 
         public static DataAccessResponse<IEnumerable<Models.PurchasedItem>> ToModel(this DataAccessResponse<IEnumerable<Entities.PurchasedItem>> response)
         {
+            if (response == null || response.Payload == null) return null;
             return new DataAccessResponse<IEnumerable<Models.PurchasedItem>>(response.Payload.ToModel(), response.Status);
         }
 
         public static DataAccessResponse<string> ToModel(this DataAccessResponse<string> response)
         {
+            if (response == null || response.Payload == null) return null;
             return new DataAccessResponse<string>(response.Payload, response.Status);
         }
     }
