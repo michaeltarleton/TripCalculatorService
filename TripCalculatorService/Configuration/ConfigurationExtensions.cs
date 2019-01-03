4b8c515c08495ce0615f65232e2bfcc3fd5da789
Michael Tarleton <michael.tarleton@me.com>
2019-01-02 22:51:06 -0600

    master: Added Reactive extensions to handle Elasticsearch readiness

--- TripCalculatorService/Configuration/ConfigurationExtensions.cs
+++ TripCalculatorService/Configuration/ConfigurationExtensions.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Nest;
 using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using TripCalculatorService;
 using TripCalculatorService.Entities;
 
@@ -56,20 +59,37 @@ namespace TripCalculatorService.Configuration
             AppSettings    settings = app.ApplicationServices.GetService<AppSettings>();
             string         index    = settings != null &&
                                       settings.ElasticConfig != null &&
-                                      string.IsNullOrWhiteSpace(settings.ElasticConfig.DefaultIndex)
-                                      ? settings.ElasticConfig.DefaultIndex
-                                      : "friends";
+                                      string.IsNullOrWhiteSpace(settings.ElasticConfig.DefaultIndex) ?
+                                      settings.ElasticConfig.DefaultIndex :
+                                      "friends";
 
-            // Remove then create the index
-            client.DeleteIndex(index);
-
-            client.CreateIndex(index, c => c.Mappings(ms => ms.Map<Friend> (m => m.AutoMap())));
-
-            // Bulk insert random friends; bulk for performance
-
-            client.Bulk((s) => {
-                return s.IndexMany(Friend.BuildRandomFriends(10));
+            var observable = Observable.Create<bool> (async o => {
+                var elasticHealth = await client.CatHealthAsync();
+                if (elasticHealth.IsValid)
+                {
+                    Console.WriteLine("Elasticsearch is UP!");
+                    o.OnNext(true);
+                    o.OnCompleted();
+                }
+                else
+                {
+                    Console.WriteLine("Elasticsearch is down!");
+                    o.OnError(new Exception("Elasticsearch is down"));
+                }
             });
+
+            var observer = Observer.Create<bool> (isElasticsearchUp => {
+                // Remove then create the index
+                client.DeleteIndex(index);
+                client.CreateIndex(index, c => c.Mappings(ms => ms.Map<Friend> (m => m.AutoMap())));
+
+                // Bulk insert random friends; bulk for performance
+                client.Bulk((s) => {
+                    return s.IndexMany(Friend.BuildRandomFriends(10));
+                });
+            });
+
+            observable.DelaySubscription(TimeSpan.FromSeconds(1)).Retry(60).Subscribe(observer);
         }
     }
 }
